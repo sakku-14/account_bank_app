9292346a49205a7baeb269117ee4026e1bed0dbd
sakku14 <kumasan.04.24.14@gmail.com>
2022-08-02 19:56:34 +0900

    add message when transactionList is empty

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -13,31 +13,48 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 600,
-      child: ListView.builder(
-        itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-              leading: CircleAvatar(
-                radius: 30,
-                child: Padding(
-                    padding: const EdgeInsets.all(6),
-                    child: FittedBox(
-                      child: Text(transactions[index].amount.toString()),
-                    )),
-              ),
-              title: Text(transactions[index].title),
-              subtitle: Text(
-                DateFormat.yMMMd().format(transactions[index].date),
-              ),
-              trailing: IconButton(
-                onPressed: () => deleteTransaction(transactions[index].id),
-                icon: const Icon(Icons.delete),
-              ),
+      child: transactions.isEmpty
+          ? Column(
+              children: <Widget>[
+                const Text('No Transaction'),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
+                )
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                          padding: const EdgeInsets.all(6),
+                          child: FittedBox(
+                            child: Text(transactions[index].amount.toString()),
+                          )),
+                    ),
+                    title: Text(transactions[index].title),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(transactions[index].date),
+                    ),
+                    trailing: IconButton(
+                      onPressed: () =>
+                          deleteTransaction(transactions[index].id),
+                      icon: const Icon(Icons.delete),
+                    ),
+                  ),
+                );
+              },
+              itemCount: transactions.length,
             ),
-          );
-        },
-        itemCount: transactions.length,
-      ),
     );
   }
 }
