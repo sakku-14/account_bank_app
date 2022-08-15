5108bcd23c1779606a38bb4812324cbb29ce5e39
sakku14 <kumasan.04.24.14@gmail.com>
2022-08-15 15:33:51 +0900

    make variable in transaction_list.dart

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -8,50 +8,61 @@ class TransactionList extends StatelessWidget {
   final Function deleteTransaction;
   final bool isLandscape;
 
-  TransactionList(this.transactions, this.deleteTransaction, this.isLandscape);
+  const TransactionList(
+      this.transactions, this.deleteTransaction, this.isLandscape);
+
+  // 個別のトランザクション
+  Widget eachTransaction(int index) {
+    return Card(
+      child: ListTile(
+        leading: CircleAvatar(
+          radius: 30,
+          child: Padding(
+              padding: const EdgeInsets.all(6),
+              child: FittedBox(
+                child: Text(transactions[index].amount.toString()),
+              )),
+        ),
+        title: Text(transactions[index].title),
+        subtitle: Text(
+          DateFormat.yMMMd().format(transactions[index].date),
+        ),
+        trailing: IconButton(
+          onPressed: () => deleteTransaction(transactions[index].id),
+          icon: const Icon(Icons.delete),
+        ),
+      ),
+    );
+  }
+
+  // トランザクションが空の時の表示
+  Widget showEmpty(BoxConstraints constraints) {
+    return Column(
+      children: <Widget>[
+        const Text('No Transaction'),
+        const SizedBox(
+          height: 20,
+        ),
+        SizedBox(
+          height: constraints.maxHeight * 0.6,
+          child: Image.asset(
+            'assets/images/waiting.png',
+            fit: BoxFit.cover,
+          ),
+        )
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
         ? LayoutBuilder(builder: (context, constraints) {
-            return Column(
-              children: <Widget>[
-                const Text('No Transaction'),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  height: constraints.maxHeight * 0.6,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
-                  ),
-                )
-              ],
-            );
+            return showEmpty(constraints);
           })
         : ListView.builder(
             itemBuilder: (context, index) {
-              return Card(
-                child: ListTile(
-                  leading: CircleAvatar(
-                    radius: 30,
-                    child: Padding(
-                        padding: const EdgeInsets.all(6),
-                        child: FittedBox(
-                          child: Text(transactions[index].amount.toString()),
-                        )),
-                  ),
-                  title: Text(transactions[index].title),
-                  subtitle: Text(
-                    DateFormat.yMMMd().format(transactions[index].date),
-                  ),
-                  trailing: IconButton(
-                    onPressed: () => deleteTransaction(transactions[index].id),
-                    icon: const Icon(Icons.delete),
-                  ),
-                ),
-              );
+              return eachTransaction(index);
             },
             itemCount: transactions.length,
           );
