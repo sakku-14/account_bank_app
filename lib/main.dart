916f05385d04aac13b9e4970d53e58569e15fd0a
sakku14 <kumasan.04.24.14@gmail.com>
2022-10-10 22:19:49 +0900

    declare provider for repository

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,11 @@ import 'package:account_book_app/view_layer/widgets/transaction_list.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/adapters.dart';
 
 import 'domain_layer/models/transaction_aggregate/transaction.dart';
+import 'infrastructure_layer/repositories/transaction_aggregate/transaction_repository.dart';
 
 const String transactionsName = 'transactions';
 
@@ -16,6 +18,11 @@ Future<void> main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(TransactionAdapter());
   await Hive.openBox<Transaction>(transactionsName);
+
+  final transactionRepository = Provider<TransactionRepository>((ref) {
+    return TransactionRepository(transactionsName);
+  });
+
   runApp(const MyApp());
 }
 
